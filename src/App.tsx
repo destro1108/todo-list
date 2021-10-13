6dd3b3ff81e00d0a113eed8e34ed7d90232b9213
D3STR011 <swanandev@gmail.com>
2021-10-13 14:48:25 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header text-red-500">XSF</header>
+    </div>
+  );
+}
+
+export default App;
